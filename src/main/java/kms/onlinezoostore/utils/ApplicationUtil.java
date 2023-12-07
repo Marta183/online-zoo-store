c17799996de33f988995d69dce3ee0fe7a82fce2
Marta183 <marta.ilchenko95@gmail.com>
2023-12-07 15:58:41 +0100

    changed creating an app link

--- src/main/java/kms/onlinezoostore/utils/ApplicationUtil.java
+++ src/main/java/kms/onlinezoostore/utils/ApplicationUtil.java
@@ -4,8 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 
 public class ApplicationUtil {
     public static String applicationUrl(HttpServletRequest request) {
-        return "http://" + request.getServerName() + ":"
-                + request.getServerPort()
+        return "http://" + request.getServerName()
+                //+ ":" + request.getServerPort()
                 + request.getContextPath();
     }
 }
