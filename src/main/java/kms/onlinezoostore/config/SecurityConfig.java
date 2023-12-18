bdec8deb32298fa5b03cec7b78ff1e34a1480557
Marta183 <marta.ilchenko95@gmail.com>
2023-12-18 16:07:38 +0100

    Allow CORS with Spring Security

--- src/main/java/kms/onlinezoostore/config/SecurityConfig.java
+++ src/main/java/kms/onlinezoostore/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -49,12 +50,13 @@ public class SecurityConfig {
             "/swagger-ui.html",
             "/swagger-resources",
             "/swagger-resources/**",
-            "/webjars/**    "
+            "/webjars/**"
     };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request ->
                         request.requestMatchers(PUBLIC_URLs).permitAll()
